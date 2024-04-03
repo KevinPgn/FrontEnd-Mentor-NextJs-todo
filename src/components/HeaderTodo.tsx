97725c055aed67270ac38a58d7d9f4d34d053ed5
KevinPgn <kevin.pigeon2810@gmail.com>
2024-04-03 21:30:48 +0200

    theme

--- src/components/HeaderTodo.tsx
+++ src/components/HeaderTodo.tsx
@@ -1,12 +1,30 @@
+"use client"
+
+import { useFiltered } from "@/store";
 import "../styles/HeaderTodo.css";
 
 import { IoSunny } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa6";
+import { useEffect } from "react";
 
 export const HeaderTodo = () => {
+  const theme = useFiltered((state) => state.theme);
+  const setTheme = useFiltered((state) => state.setTheme);
+  
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme])
+
   return <>
     <div className="header-todo">
         <h1>TODO</h1>
-        <IoSunny />
+        <div className="theme-toggle">
+          {theme === "dark" ? (
+            <IoSunny onClick={() => setTheme("light")} />
+          ) : (
+            <FaMoon onClick={() => setTheme("dark")} />
+          )}
+        </div>
     </div>
   </>
 }
--- src/store.ts
+++ src/store.ts
@@ -4,10 +4,11 @@ interface Boolean {
   isActive: boolean;
   isCompleted: boolean;
   all: boolean;
+  theme: string;
   setActive: () => void;
   setCompleted: () => void;
   setAll: () => void;
-  
+  setTheme: (theme: string) => void;
 }
 /*
   when i click on the completed button, isCompleted should be true but isActive should be false
@@ -20,7 +21,9 @@ export const useFiltered = create<Boolean>((set) => ({
   isActive: false,
   isCompleted: false,
   all: true,
+  theme: "dark",
   setActive: () => set((state) => ({isActive: true, isCompleted: false, all: false})),
   setCompleted: () => set((state) => ({isActive: false, isCompleted: true, all: false})),
   setAll: () => set((state) => ({isActive: false, isCompleted: false, all: true})),
+  setTheme: (theme: string) => set((state) => ({theme: theme}))
 }))
